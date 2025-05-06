9cc7e997df041220b28c7830173a0697492fb8b2
Ivon <ivonhuaringach@gmail.com>
2025-05-06 01:17:24 -0500

    se actualizó diseño

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { UsuarioComponent } from './components/usuario/usuario.component';
 import { EventoComponent } from './components/evento/evento.component';
 
 export const appRoutes: Routes = [
-  { path: '', redirectTo: '/Usuario', pathMatch: 'full' }, // Redirige a 'Usuario' por defecto
+  { path: '', component: UsuarioComponent }, // Redirige a 'Usuario' por defecto
   { path: 'Usuario', component: UsuarioComponent },
   { path: 'Evento', component: EventoComponent },
 ];
